938a33aafdc07bf1b5e4f4edf2d05d2f79abd065
blake <1466823029@qq.com>
2020-04-07 19:43:47 +0800

    2020 0407 company

---
+++ TestCases/test_SeeNews.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/4/7 18:51
+# @File    : test_SeeNews.py
+class TestSeeNews(object):
+
+    def test_see_news(self, driver):
+
+        print("test")
+        import time
+        time.sleep(5)
---
+++ data/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/4/7 18:41
+# @File    : __init__.py.py
